9bf1ab20c6b62225fcb0ff745dd3c3e606baa3fa
UglukFearless <UglukFearless@mail.ru>
2021-04-18 23:47:54 +0700

    lesson about led control by potentiometer

---
+++ 003_variable_brightness/variable_brightness/variable_brightness.ino
@@ -0,0 +1,19 @@
+#define LED_PIN 9
+#define POT_PIN A0
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(POT_PIN, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int rotation = 0;
+  int brightness = 0;
+
+  rotation = analogRead(POT_PIN);
+  brightness = rotation/4;
+
+  analogWrite(LED_PIN, brightness);
+}
