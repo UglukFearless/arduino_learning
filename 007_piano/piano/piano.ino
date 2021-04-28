9bf04d85deefc269d115b20fa7d2b43d02f1616e
UglukFearless <UglukFearless@mail.ru>
2021-04-28 23:49:36 +0700

    set frequency of notes

--- 007_piano/piano/piano.ino
+++ 007_piano/piano/piano.ino
@@ -1,6 +1,9 @@
 #define BUZZER_PIN 13
-#define FIRST_KEY_PIN 7
-#define KEY_COUNT 3 
+#define KEY_1_PIN 2
+#define KEY_2_PIN 4
+#define KEY_3_PIN 7
+#define KEY_4_PIN 8
+#define KEY_5_PIN 9
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,15 +12,33 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i < KEY_COUNT; ++i) {
-    int keyPin = i + FIRST_KEY_PIN;
 
-    boolean keyUp = digitalRead(keyPin);
+  // read input states
+  boolean keyUp1 = !digitalRead(KEY_1_PIN);
+  boolean keyUp2 = !digitalRead(KEY_2_PIN);
+  boolean keyUp3 = !digitalRead(KEY_3_PIN);
+  boolean keyUp4 = !digitalRead(KEY_4_PIN);
+  boolean keyUp5 = !digitalRead(KEY_5_PIN);
 
-    if (!keyUp) {
-      int frequency = 440 + i * 50;
-
-      tone(BUZZER_PIN, frequency, 20);
-    }
+  if (keyUp1) {
+    // A
+    int frequency = 440;
+    tone(BUZZER_PIN, frequency, 20);
+  } else if (keyUp2) {
+    // G
+    int frequency = 392;
+    tone(BUZZER_PIN, frequency, 20);
+  } else if (keyUp3) {
+    // F
+    int frequency = 349;
+    tone(BUZZER_PIN, frequency, 20);
+  } else if (keyUp4) {
+    // E
+    int frequency = 330;
+    tone(BUZZER_PIN, frequency, 20);
+  } else if (keyUp5) {
+    // D
+    int frequency = 294;
+    tone(BUZZER_PIN, frequency, 20);
   }
 }
